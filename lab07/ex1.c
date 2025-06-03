2a2b0e614b28b5a356cf60f23bdff8eb8533f2e4
AlenasRed <alenasred@hotmail.com>
2025-06-03 12:53:40 +0200

    lab7

--- lab07/ex1.c
+++ lab07/ex1.c
@@ -53,8 +53,25 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
 
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
+        int32_t sum_arr[4];
+        __m128i sum_vec = _mm_setzero_si128();
+        __m128i tmp = _mm_setzero_si128();
+        __m128i cmp = _mm_setzero_si128();
 
-        /* Hint: you'll need a tail case. */
+        for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            tmp = _mm_loadu_si128((__m128i*)&vals[i]);
+            cmp = _mm_cmpgt_epi32(tmp, _127);
+            tmp = _mm_and_si128(tmp, cmp);
+            sum_vec = _mm_add_epi32(sum_vec, tmp);
+        }
+            _mm_storeu_si128((__m128i*)sum_arr, sum_vec);
+            result += sum_arr[0] + sum_arr[1] + sum_arr[2] + sum_arr[3];
+
+            for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+                if (vals[i] >= 128) {
+                    result += vals[i];
+                }
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
@@ -72,8 +89,35 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
         /* Copy your sum_simd() implementation here, and unroll it */
+        int32_t sum_arr[4];
+        __m128i sum_vec = _mm_setzero_si128();
+        __m128i tmp1 = _mm_setzero_si128();
+        __m128i tmp2 = _mm_setzero_si128();
+        __m128i cmp1 = _mm_setzero_si128();
+        __m128i cmp2 = _mm_setzero_si128();
 
-        /* Hint: you'll need 1 or maybe 2 tail cases here. */
+        for(unsigned int i = 0; i < NUM_ELEMS / 8 * 8; i += 8) {
+            tmp1 = _mm_loadu_si128((__m128i*)&vals[i]);
+            tmp2 = _mm_loadu_si128((__m128i*)&vals[i + 4]);
+
+            cmp1 = _mm_cmpgt_epi32(tmp1, _127);
+            cmp2 = _mm_cmpgt_epi32(tmp2, _127);
+
+            tmp1 = _mm_and_si128(tmp1, cmp1);
+            tmp2 = _mm_and_si128(tmp2, cmp2);
+
+            sum_vec = _mm_add_epi32(sum_vec, tmp1);
+            sum_vec = _mm_add_epi32(sum_vec, tmp2);
+        }
+
+        _mm_storeu_si128((__m128i*)sum_arr, sum_vec);
+        result += sum_arr[0] + sum_arr[1] + sum_arr[2] + sum_arr[3];
+
+        for (unsigned int i = NUM_ELEMS / 8 * 8; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
--- lab07/ex2.c
+++ lab07/ex2.c
@@ -1,4 +1,5 @@
 #include "ex2.h"
+#include <omp.h>
 
 double dotp_naive(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
@@ -11,6 +12,12 @@ double dotp_naive(double* x, double* y, int arr_size) {
 double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
+    #pragma omp parallel for
+    for (int i = 0; i < arr_size; i++)
+    {
+        #pragma omp critical
+        global_sum += x[i] * y[i];
+    }
     // Use the critical keyword here!
     return global_sum;
 }
@@ -18,8 +25,11 @@ double dotp_critical(double* x, double* y, int arr_size) {
 // Reduction Keyword
 double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
-    // TODO: Implement this function
-    // Use the reduction keyword here!
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i = 0; i < arr_size; i++)
+    {
+        global_sum += x[i] * y[i];
+    }
     return global_sum;
 }
 
@@ -27,13 +37,30 @@ double dotp_reduction(double* x, double* y, int arr_size) {
 double dotp_manual_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
+    int num_threads = omp_get_max_threads();
+    double thread_sums[num_threads];
+
+     for (int i = 0; i < num_threads; i++) {
+        thread_sums[i] = 0.0;
+    }
     // Do NOT use the `reduction` directive here!
     #pragma omp parallel
     {
-        double local_sum = 0.0;
         // TODO: Parallel Section
-
-        // TODO: Critical Section
+        int current_thread = omp_get_thread_num();
+         double local_sum = 0.0;
+        #pragma omp for
+        for (int i = 0; i < arr_size; i++)
+        {
+            local_sum += x[i] * y[i];
+        }
+        thread_sums[current_thread] = local_sum;
     }
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        global_sum += thread_sums[i];
+    }
+
     return global_sum;
 }
